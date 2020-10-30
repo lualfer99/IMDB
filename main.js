7c6337139bd926633aa4748ff3e2aa2f65bbc3ab
surikat0 <urik.landa@hotmail.com>
2020-10-30 15:40:25 +0100

    nuevas modificaciones de testeo

--- main.js
+++ main.js
@@ -9,14 +9,14 @@ var actor3 = new profesional_1.Professional("Brad Pitt", 55, "Man", 80, 1.84, "B
 var actor4 = new profesional_1.Professional("Joaquin Phoenix", 46, "Man", 71, 1.73, "Brown", "Green", "White", false, "American", 1, "Actor");
 var actor5 = new profesional_1.Professional("John Legend", 41, "Man", 65, 1.75, "Brown", "Brown", "Black", false, "American", 1, "Musician");
 var actriz1 = new profesional_1.Professional("Emma Stone", 31, "Woman", 55, 1.68, "Red", "Blue", "White", false, "American", 1, "Actress");
-var actriz2 = new profesional_1.Professional("Scarlett Johannsson", 35, "Woman", 56, 1.60, "Brown", "Green", "White", false, "American", 1, "Actress");
+var actriz2 = new profesional_1.Professional("Scarlett Johannsson", 35, "Woman", 56, 1.60, "Blond", "Green", "White", false, "American", 1, "Actress");
 var actriz3 = new profesional_1.Professional("Rooney Mara", 35, "Woman", 54, 1.65, "Brown", "Green", "White", false, "American", 0, "Actress");
-var cast1 = [actor1.name, actriz2.name, actor3.name];
-var cast2 = [actor4.name, actriz2.name, actriz3.name];
-var cast3 = [actor2.name, actriz1.name, actor5.name];
+var castBigShort = [actor1.name, actriz2.name, actor3.name];
+var castHer = [actor4.name, actriz2.name, actriz3.name];
+var castLaLa = [actor2.name, actriz1.name, actor5.name];
 // "The Big Short" Movie
 var movie1 = new movie_1.Movie("The Big Short", 2015, "EEUU", "Based on Real Events");
-movie1.actors = cast1;
+movie1.actors = castBigShort;
 movie1.director = "Adam McKay";
 movie1.writer = "Adam McKay";
 movie1.language = "English";
@@ -26,7 +26,7 @@ movie1.mostrarPeliculas();
 console.log("---------------");
 // "Her" movie.
 var movie2 = new movie_1.Movie("Her", 2013, "EEUU", "Romance");
-movie2.actors = cast2;
+movie2.actors = castHer;
 movie2.director = "Spike Jonze";
 movie2.writer = "Spike Jonze";
 movie2.language = "English";
@@ -36,7 +36,7 @@ movie2.mostrarPeliculas();
 console.log("---------------");
 // "La la land" Movie.
 var movie3 = new movie_1.Movie("La la land", 2016, "EEUU", "Musical");
-movie3.actors = cast3;
+movie3.actors = castLaLa;
 movie3.director = "Damien Chazelle";
 movie3.writer = "Damien Chazelle";
 movie3.language = "English";
--- main.ts
+++ main.ts
@@ -9,18 +9,18 @@ let actor4:Professional = new Professional("Joaquin Phoenix", 46, "Man", 71, 1.7
 let actor5:Professional = new Professional("John Legend", 41, "Man", 65, 1.75, "Brown", "Brown", "Black", false, "American", 1, "Musician");
 
 let actriz1:Professional = new Professional("Emma Stone", 31, "Woman", 55, 1.68, "Red", "Blue", "White", false, "American", 1, "Actress");
-let actriz2:Professional = new Professional("Scarlett Johannsson", 35, "Woman", 56, 1.60, "Brown", "Green", "White", false, "American", 1, "Actress");
+let actriz2:Professional = new Professional("Scarlett Johannsson", 35, "Woman", 56, 1.60, "Blond", "Green", "White", false, "American", 1, "Actress");
 let actriz3:Professional = new Professional("Rooney Mara", 35, "Woman", 54, 1.65, "Brown", "Green", "White", false, "American", 0, "Actress");
 
-let cast1 = [actor1.name, actriz2.name, actor3.name];
-let cast2 = [actor4.name, actriz2.name, actriz3.name];
-let cast3 = [actor2.name, actriz1.name, actor5.name];
+let castBigShort = [actor1.name, actriz2.name, actor3.name];
+let castHer = [actor4.name, actriz2.name, actriz3.name];
+let castLaLa = [actor2.name, actriz1.name, actor5.name];
 
 // "The Big Short" Movie
 
 let movie1:Movie = new Movie("The Big Short", 2015, "EEUU", "Based on Real Events");
 
-movie1.actors = cast1;
+movie1.actors = castBigShort;
 movie1.director = "Adam McKay";
 movie1.writer = "Adam McKay";
 movie1.language = "English";
@@ -33,7 +33,7 @@ console.log("---------------");
 
 let movie2:Movie = new Movie("Her", 2013, "EEUU", "Romance");
 
-movie2.actors = cast2;
+movie2.actors = castHer;
 movie2.director = "Spike Jonze";
 movie2.writer = "Spike Jonze";
 movie2.language = "English";
@@ -46,7 +46,7 @@ console.log("---------------");
 
 let movie3:Movie = new Movie("La la land", 2016, "EEUU", "Musical");
 
-movie3.actors = cast3;
+movie3.actors = castLaLa;
 movie3.director = "Damien Chazelle";
 movie3.writer = "Damien Chazelle";
 movie3.language = "English";
